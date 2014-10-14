4fa7e09894f5be6b896cecfe15a1a3a49d5d7593
Alessandro Chelli <alessandrochelli@gmail.com>
2014-10-14 17:39:36 +0200

    Create the repository

---
+++ LWM Objects/emptyTemplate/emptyTemplate.ino
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+
+//librerie per lwm
+#include "lwm.h";
+#include "lwm/sys/sys.h"
+#include "lwm/nwk/nwk.h"
+
+#include "ApioLwm.h"
+
+
+void setup() {
+  Serial1.begin(115200); //riga di debug
+  apioSetup();
+  apioSend("2:hi:2:l-");
+}
+
+void loop() {
+  SYS_TaskHandler();
+  select();
+}
+
---
+++ Xbee Objects/emptyTemplate/emptyTemplate.ino
@@ -0,0 +1,26 @@
+#include <XBee.h>
+#include "ApioXBee.h"
+
+
+
+
+void setup()  
+{ 
+  Serial.begin(9600);
+  apioSetupMega();
+  //Here we assume that the objectID is 4545 and physical address is 40B3E1D4
+  apioSend("4545:hi:40B3E1D4:z-"); //Send init for my object
+
+} 
+
+void loop()  
+{
+  //This is the function that we call in each loop and let you receive key value
+  apioReceive();
+  
+  ///Azioni che devono generare un output.
+  
+  /*---------------------------*/
+  //Azioni che provengono dagli Input
+ 
+}
